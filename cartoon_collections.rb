7469959b8d2f5741ae1a11390d8f1429d222ae80
Philippe Timothe <philippetimothe@gmail.com>
2020-03-14 16:57:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
    cheese_types = ["cheddar", "gouda", "camembert"]
  i = 0
  while i < array.length
- return array[i] if yield
+ return array[i] if yield(array[i])
  i = i + 1
 end
 end
