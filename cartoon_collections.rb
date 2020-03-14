258529904ff2fcad934dd4866f729752b20abf3b
Philippe Timothe <philippetimothe@gmail.com>
2020-03-14 16:38:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,8 @@ end
 def find_the_cheese(array)
    cheese_types = ["cheddar", "gouda", "camembert"]
  
+ 
+ 
  array.find do |x| x == ("cheddar", "gouda", "camembert")
  array.include?("cheddar", "gouda", "camembert")
 end
